679a07e827df353d729567eb7f1baebc6d1b4176
Vijay Menon <vijay.menon@radnet.com>
2020-10-03 19:34:01 -0700

    Update

--- buy_and_sell_stock_twice.py
+++ buy_and_sell_stock_twice.py
@@ -8,11 +8,12 @@ def buy_and_sell_stock_twice(prices):
         min_price_so_far = min(min_price_so_far, price)
         max_total_profit = max(max_total_profit, price - min_price_so_far)
         first_buy_sell_profits[i] = max_total_profit
-        print(first_buy_sell_profits)
+        #print(first_buy_sell_profits)
 
     # Backward phase.  For each day, find the maximum profit if we make the second buy 
     # on that day
     max_price_so_far = float('-inf')
+   
     for i, price in reversed(list(enumerate(prices[1:], 1))):
         max_price_so_far = max(max_price_so_far, price)
         max_total_profit = max(max_total_profit, max_price_so_far - price + first_buy_sell_profits[i-1])
